4831a5a9d9943a05d9374f700dbd984e483a344e
BobCochran <r2cochran2@gmail.com>
2019-09-30 20:07:15 -0400

    Trying out Paige Niedringhaus method using readline

--- reformatfile.js
+++ reformatfile.js
@@ -1,7 +1,43 @@
-/* File system object */
+/* Code to reformat a file line by line into a json
+ * string. A lot of this code was copied from Paige
+ * Niedringhaus' post appearing in 
+ * https://itnext.io/using-node-js-to-read-really-really-large-files-pt-1-d2057fe76b33
+ * */
 var fs = require('fs')
 
-data = fs.readFileSync('csvdata2.csv')
+var readline = require('readline')
 
-console.log(data.toString('utf8'))
+var stream = require('stream')
+
+var instream = fs.createReadStream('csvdata2.csv')
+
+var outstream = new stream()
+
+var rl = readline.createInterface(instream, outstream)
+
+var myFlds = [];
+
+//Count number of lines read in
+var lineCount = 0;
+
+rl.on('line', function(line) {
+
+	//increment line counter
+	lineCount++
+
+	//split the line on commas
+
+	var flds = line.split(',')
+
+	myFlds.push(flds)
+} )
+
+rl.on('close', function() {
+
+	console.log("The line count is " + lineCount)
+	console.log("Example of what is in myFlds" + myFlds[1])
+
+})
+
+//console.log(data.toString('utf8'))
 
